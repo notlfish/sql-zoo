6767f5221f156a4df12581b5cfd12cb04572c730
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-05-25 19:19:56 -0300

    Complete tutorial 8

---
+++ 8-null.sql
@@ -0,0 +1,51 @@
+-- List the teachers who have NULL for their department.
+select name from teacher where dept is null;
+
+-- Note the INNER JOIN misses the teachers with no department and
+-- the departments with no teacher.
+SELECT teacher.name, dept.name
+FROM teacher
+INNER JOIN dept ON (teacher.dept=dept.id);
+
+-- Use a different JOIN so that all teachers are listed.
+SELECT teacher.name, dept.name
+FROM teacher
+LEFT JOIN dept ON (teacher.dept=dept.id);
+
+-- Use a different JOIN so that all departments are listed.
+SELECT teacher.name, dept.name
+FROM teacher
+RIGHT JOIN dept ON (teacher.dept=dept.id);
+
+-- Use COALESCE to print the mobile number. Use the number '07986 444 2266'
+-- if there is no number given. Show teacher name and mobile number or
+-- '07986 444 2266'
+SELECT teacher.name, coalesce(mobile, '07986 444 2266') FROM teacher;
+
+-- Use the COALESCE function and a LEFT JOIN to print the teacher name and
+-- department name. Use the string 'None' where there is no department
+SELECT teacher.name, coalesce(dept.name, 'None')
+FROM teacher
+LEFT JOIN dept ON (teacher.dept=dept.id);
+
+-- Use COUNT to show the number of teachers and the number of mobile phones.
+select count(name), count(mobile) from teacher;
+
+-- Use COUNT and GROUP BY dept.name to show each department and the number of
+-- staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.
+SELECT dept.name, COUNT(teacher.name)
+FROM teacher
+RIGHT JOIN dept ON (teacher.dept=dept.id)
+GROUP BY dept.name;
+
+-- Use CASE to show the name of each teacher followed by 'Sci' if the
+-- teacher is in dept 1 or 2 and 'Art' otherwise.
+select name, case when dept in(1, 2) then 'Sci' else 'Art' end
+from teacher;
+
+-- Use CASE to show the name of each teacher followed by 'Sci' if the teacher is
+-- in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
+select name, case when dept in(1, 2) then 'Sci'
+                  when dept = 3 then 'Art'
+                  else 'None' end
+from teacher;
